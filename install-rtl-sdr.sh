44a782b0d148612e900dd8d8d20019c62282c6d7
Lobethal Lutheran School <79490741+Lobethal-Lutheran-School@users.noreply.github.com>
2021-02-26 19:15:56 +1030

    Update install-rtl-sdr.sh

--- install-rtl-sdr.sh
+++ install-rtl-sdr.sh
@@ -1,6 +1,8 @@
-#1/bin/sh
-#configure software needed for RTL-SDR use 
+#!/bin/sh
+#configure software needed for RTL-SDR use
 sudo apt-get install gqrx-sdr rtl-sdr librtlsdr-dev
 sudo wget -O /etc/udev/rules.d/20-rtlsdr.rules http://raw.githubusercontent.com/osmocom/rtl-sdr/master/rtl-sdr.rules
+
+#need to blacklist default module providing TV tuner supprot, to allow module providing SDR functionality to work
 cat blacklist-dvb_usb_rtl28xxu.txt | sudo tee -a /etc/modprobe.d/blacklist-rtl.conf
 echo 'blacklisting default vb_usb_rtl28xxu module to allow SDR use'
