fdd0d25beae50329f5badeeb4d59e3f59bcf0ea1
Lobethal Lutheran School <79490741+Lobethal-Lutheran-School@users.noreply.github.com>
2021-03-03 17:21:20 +1030

    Update install-minetest.sh to include python3-pip

--- install-minetest.sh
+++ install-minetest.sh
@@ -2,10 +2,13 @@
 
 sudo apt-get install minetest fonts-crosextra-caladea fonts-crosextra-carlito minetest-mod-moreblocks minetest-mod-moreores minetest-mod-pipeworks minetest-server minetestmapper
 
-sudo apt-get install luajit lua-socket lua-cjson idle3
+sudo apt-get install luajit lua-socket lua-cjson idle3 python3-pip
 
 pip3 install miney
 
 # install mineysocket mod in minetest
+# cd /var/games/minetest-server/.minetest/mods
+# git clone git@github.com:miney-py/mineysocket.git
+
 # Needs "secure.trusted_mods = mineysocket" in minetest.conf
 # on starting minetest game, ensure mods are activated under configure tab
